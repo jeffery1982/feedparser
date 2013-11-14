f784e9a44b58e3267d007bedab60723094a171a2
Jeffery <jeffery821217@gmail.com>
2013-11-13 22:48:58 -0800

    Refactoring the code a little bit.

--- src/com/notenet/feedparser/execute/HttpESHelper.java
+++ src/com/notenet/feedparser/execute/HttpESHelper.java
@@ -11,7 +11,6 @@ import java.nio.charset.Charset;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -26,7 +25,7 @@ public class HttpESHelper {
 	private final String USER_AGENT = "Mozilla/5.0";
 	 
 	public static void main(String[] args) throws Exception {
-		HttpESHelper http = new HttpESHelper();
+		//HttpESHelper http = new HttpESHelper();
 		//http.createIndex("feedindex");
 		//http.putMapping("feedindex", "feed");
 		//http.addIndex("feedindex", "feed", 1);
@@ -35,34 +34,43 @@ public class HttpESHelper {
 	
 	public void createIndex(String indexName) throws Exception {
 		String url = String.format("http://localhost:9200/%s", indexName);
-		HttpClient client= new DefaultHttpClient();
-		HttpPost request = new HttpPost(url);
-		HttpEntity entity = new StringEntity(FileUtils.readFileToString(new File("testdata/setting")));
-		request.setEntity(entity);
-		HttpResponse result = client.execute(request);
-		HttpEntity resultEntity = result.getEntity();
-		InputStream inputStream = resultEntity.getContent();
-		byte[] responseBody = IOUtils.toByteArray(inputStream);
-		String resultString = new String(responseBody);
+		String resultString = this.sendPost(url, FileUtils.readFileToString(new File("testdata/setting")));
 		System.out.println(resultString);
 	}
 	
 	public void putMapping(String indexName, String type) throws UnsupportedEncodingException, IOException {
 		String url = String.format("http://localhost:9200/%s/%s/_mapping", indexName, type);
+		String resultString = this.sendPost(url, FileUtils.readFileToString(new File("testdata/mappings")));
+		System.out.println(resultString);
+	}
+	
+	public String getIndex(String indexName, String type, String id) throws ClientProtocolException, IOException {
+		String url = String.format("http://localhost:9200/%s/%s/%s", indexName, type, id);
+		String resultString = this.sendGet(url);
+		return resultString;
+	}
+	
+	public void addIndex(String indexName, String type, String id, String indexEntity) throws UnsupportedEncodingException, IOException {
+		System.out.println("Put into index for index name: " + indexName);
+		String url = String.format("http://localhost:9200/%s/%s/%s", indexName, type, id);
+		String resultString = this.sendPost(url, indexEntity);
+		System.out.println(resultString);
+	}
+	
+	private String sendPost(String url, String contentEntity) throws ClientProtocolException, IOException {
 		HttpClient client= new DefaultHttpClient();
 		HttpPost request = new HttpPost(url);
-		HttpEntity entity = new StringEntity(FileUtils.readFileToString(new File("testdata/mappings")));
+		HttpEntity entity = new StringEntity(contentEntity, Charset.forName("UTF-8") );//FileUtils.readFileToString(new File("testdata/sampledoc")));
 		request.setEntity(entity);
 		HttpResponse result = client.execute(request);
 		HttpEntity resultEntity = result.getEntity();
 		InputStream inputStream = resultEntity.getContent();
 		byte[] responseBody = IOUtils.toByteArray(inputStream);
 		String resultString = new String(responseBody);
-		System.out.println(resultString);
+		return resultString;
 	}
 	
-	public String getIndex(String indexName, String type, String id) throws ClientProtocolException, IOException {
-		String url = String.format("http://localhost:9200/%s/%s/%s", indexName, type, id);
+	private String sendGet(String url) throws ClientProtocolException, IOException {
 		HttpClient client= new DefaultHttpClient();
 		HttpGet request = new HttpGet(url);
 		HttpResponse result = client.execute(request);
@@ -71,51 +79,5 @@ public class HttpESHelper {
 		byte[] responseBody = IOUtils.toByteArray(inputStream);
 		String resultString = new String(responseBody, Charset.forName("UTF-8"));
 		return resultString;
-	}
-	
-	public void addIndex(String indexName, String type, String id, String indexEntity) throws UnsupportedEncodingException, IOException {
-		System.out.println("Put into index for index name: " + indexName);
-		String url = String.format("http://localhost:9200/%s/%s/%s", indexName, type, id);
-		HttpClient client= new DefaultHttpClient();
-		//client.getParams().setParameter("http.protocol.content-charset", "UTF-8");
-		HttpPost request = new HttpPost(url);
-		HttpEntity entity = new StringEntity(indexEntity, Charset.forName("UTF-8") );//FileUtils.readFileToString(new File("testdata/sampledoc")));
-		request.setEntity(entity);
-		HttpResponse result = client.execute(request);
-		HttpEntity resultEntity = result.getEntity();
-		InputStream inputStream = resultEntity.getContent();
-		byte[] responseBody = IOUtils.toByteArray(inputStream);
-		String resultString = new String(responseBody);
-		System.out.println(resultString);
-	}
- 
-	private void sendGet() throws Exception {
-		String url = "http://www.google.com/search?q=mkyong";
-		URL obj = new URL(url);
-		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
- 
-		// optional default is GET
-		con.setRequestMethod("GET");
-		//add request header
-		con.setRequestProperty("User-Agent", USER_AGENT);
- 
-		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'GET' request to URL : " + url);
-		System.out.println("Response Code : " + responseCode);
- 
-		BufferedReader in = new BufferedReader(
-		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		StringBuffer response = new StringBuffer();
- 
-		while ((inputLine = in.readLine()) != null) {
-			response.append(inputLine);
-		}
-		in.close();
- 
-		//print result
-		System.out.println(response.toString());
- 
-	}
-
+	}	
 }
